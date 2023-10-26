08f030ffdfefd35e8f3a4cff8c56b7ff45e47b9c
Boi <cambodianjesuss@gmail.com>
2023-10-26 00:30:45 -0700

    rendering list of posts saved in state and mapped

--- src/PostList.js
+++ src/PostList.js
@@ -13,9 +13,23 @@ const PostList = () => {
         fetchPosts();
     }, []);
 
-    console.log(posts);
+    // wrap our object in an array
+    const renderedPosts = Object.values(posts).map(post => {
+        return (
+            <div 
+                className="card" 
+                style={{width: "30%", marginBottom: "20px", }}
+                key={post.id}
+                >
 
-    return <div></div>
+                <div className="card-body">
+                    <h3>{post.title}</h3>
+                </div>
+            </div>
+        )
+    })
+
+    return <div className="d-flex flex-row flex-wrap justify-content-between">{renderedPosts}</div>
 }
 
 export default PostList;
