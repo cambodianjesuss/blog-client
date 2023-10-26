946a22642d5e4b11ec8e6237c3df95036218470a
Boi <cambodianjesuss@gmail.com>
2023-10-26 00:39:58 -0700

    formatting

--- src/PostList.js
+++ src/PostList.js
@@ -2,31 +2,30 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const PostList = () => {
-    const [posts, setPosts] = useState({});
+  const [posts, setPosts] = useState({});
 
-    const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4000/posts');
-        setPosts(res.data); // axios response stored in data prop
+  const fetchPosts = async () => {
+    const res = await axios.get('http://localhost:4000/posts');
+    	setPosts(res.data); // axios response stored in data prop
     };
 
     useEffect(()=>{
-        fetchPosts();
+      fetchPosts();
     }, []);
 
     // wrap our object in an array
     const renderedPosts = Object.values(posts).map(post => {
-        return (
-            <div 
-                className="card" 
-                style={{width: "30%", marginBottom: "20px", }}
-                key={post.id}
-                >
-
-                <div className="card-body">
-                    <h3>{post.title}</h3>
-                </div>
-            </div>
-        )
+    	return (
+        <div 
+          className="card" 
+          style={{width: "30%", marginBottom: "20px", }}
+          key={post.id}
+        >
+					<div className="card-body">
+						<h3>{post.title}</h3>
+					</div>		
+        </div>
+      )
     })
 
     return <div className="d-flex flex-row flex-wrap justify-content-between">{renderedPosts}</div>
