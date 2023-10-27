2eb98248b8cdc8e58c78ed58887b092bd5df3690
Boi <cambodianjesuss@gmail.com>
2023-10-26 20:46:43 -0700

    Created the base create comment components

---
+++ src/CommentsCreate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CommentsCreate = () => {
+  return (
+    <div>
+      <form>
+        <div className='form-group'>
+          <input />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default CommentsCreate;
