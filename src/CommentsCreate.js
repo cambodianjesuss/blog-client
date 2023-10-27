51d69bbbdc6d283c1c7162b283e8f3b4da322d93
Boi <cambodianjesuss@gmail.com>
2023-10-27 11:45:08 -0700

    adding comments to posts

--- src/CommentsCreate.js
+++ src/CommentsCreate.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
-const CommentsCreate = () => {
+const CommentsCreate = ({postId}) => {
+  const [content, setContent] = useState('');
+  
+  const onSubmit = async (e)=>{
+    e.preventDefault();
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content
+    });
+
+    setContent('');
+  }
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className='form-group'>
           <label>New Comments</label>
-          <input className='form-control' />
+          <input 
+            value={content}
+            onChange={e => setContent(e.target.value)}
+            className='form-control'
+          />
         </div>
         <button className='btn btn-primary'>Submit</button>
       </form>
